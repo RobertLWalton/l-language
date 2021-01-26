7b14dae4be47dc80850f09f9cbe6272de95f09c4
Robert L Walton <walton@acm.org>
2021-01-26 06:38:13 -0500

    update

--- include/l_language.h
+++ include/l_language.h
@@ -2,25 +2,16 @@
 //
 // File:	l_language.h
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Fri Dec 11 05:10:36 EST 2020
-//
+// Date:	Sat Dec 19 06:28:20 EST 2020
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
 // for this program.
 
-// Table of Contents
-//
-//	Usage and Setup
-
-// Usage and Setup
-// ----- --- -----
-
 # ifndef L_LANGUAGE_H
 # define L_LANGUAGE_H
 
 # include <ll_parser.h>
-
 
 namespace l_language
 {
